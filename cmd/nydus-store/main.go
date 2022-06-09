2e263b2f48dfca65ff08c3b121c26ea37b8c8a10
fatelei <fatelei@gmail.com>
2022-06-09 16:53:39 +0800

    chore: add cli

--- cmd/nydus-store/main.go
+++ cmd/nydus-store/main.go
@@ -1,32 +1,60 @@
 package main
 
 import (
-	"context"
-	"github.com/containerd/nydus-snapshotter/config"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/containerd/containerd/log"
+	"github.com/containerd/nydus-snapshotter/cmd/containerd-nydus-grpc/pkg/command"
+	"github.com/containerd/nydus-snapshotter/cmd/containerd-nydus-grpc/pkg/logging"
+	"github.com/containerd/nydus-snapshotter/config"
 	"github.com/containerd/nydus-snapshotter/pkg/additionlayerstore/fs"
+	"github.com/containerd/nydus-snapshotter/pkg/errdefs"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
 )
-
-func main() {
-	ctx := context.Background()
-	mountPoint := "/var/lib/nydus-store/store"
-	var cfg config.Config
-	if err := fs.Mount(ctx, mountPoint, true, &cfg); err != nil {
-		log.G(ctx).WithError(err).Fatalf("failed to mount fs at %q", mountPoint)
-	}
-	defer func() {
-		syscall.Unmount(mountPoint, 0)
-		log.G(ctx).Info("Exiting")
-	}()
-	waitForSIGINT()
-}
 
 func waitForSIGINT() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
+}
+
+func main() {
+	flags := command.NewFlags()
+	app := &cli.App{
+		Name:    "containerd-nydus-grpc",
+		Usage:   "nydus containerd proxy snapshotter plugin",
+		Version: "0.0.0",
+		Flags:   flags.F,
+		Action: func(c *cli.Context) error {
+			if err := logging.SetUp(flags.Args.LogLevel); err != nil {
+				return errors.Wrap(err, "failed to prepare logger")
+			}
+
+			var cfg config.Config
+			if err := command.Validate(flags.Args, &cfg); err != nil {
+				return errors.Wrap(err, "invalid argument")
+			}
+
+			mountPoint := "/var/lib/nydus-store/store"
+			if err := fs.Mount(c.Context, mountPoint, true, &cfg); err != nil {
+				log.G(c.Context).WithError(err).Fatalf("failed to mount fs at %q", mountPoint)
+			}
+			defer func() {
+				syscall.Unmount(mountPoint, 0)
+				log.G(c.Context).Info("Exiting")
+			}()
+			waitForSIGINT()
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		if errdefs.IsConnectionClosed(err) {
+			log.L.Info("snapshotter exited")
+			return
+		}
+		log.L.WithError(err).Fatal("failed to start nydus snapshotter")
+	}
 }
